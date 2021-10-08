7bfb7a9418d13126c6e6141a147a26c0c2f00b33
Fabio Franzoni <franza@gmail.com>
2021-10-08 10:55:34 +0200

    Update test.js

--- pages/test.js
+++ pages/test.js
@@ -1,1 +1,23 @@
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
 
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Next.js Starter! - test page</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Header title="Welcome to my app!" />
+        <p className="description">
+          oro!
+        </p>
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
